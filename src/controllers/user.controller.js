cc2ca3745389a42b651132de41e8c072d739c5b2
Entrepreneur-Shyam <apteno.co@gmail.com>
2024-03-05 22:34:38 +0530

    Add comment ,like,playlist,and tweet

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -174,8 +174,8 @@ const logoutUser = asyncHandler(async (req, res) => {
 
   return res
     .status(200)
-    .clearCookie("accessToken", accessToken, options)
-    .clearCookie("refreshToken", refreshToken, options)
+    .clearCookie("accessToken", options)           //   .clearCookie("accessToken", accessToken, options)
+    .clearCookie("refreshToken", options)         //   .clearCookie("refreshToken", refreshToken, options)
     .json(new ApiError(200, {}, "User logged Out"));
 });
 
